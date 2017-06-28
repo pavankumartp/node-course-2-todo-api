58945698a0e72104e676adfe07fe235decb2d6f4
Pavan Kumar <pavan.kumar@sap.com>
2017-06-28 07:10:17 +0530

    adding DELETE method

--- server/server.js
+++ server/server.js
@@ -21,48 +21,59 @@ app.use(bodyParser.json());
 
 app.post('/user', (req,res)=>{
   debugger;
- var user = new User({
-   name: req.body.name,
-   email: req.body.email,
-   age: req.body.age,
-   location:req.body.location
- });
+  var user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    age: req.body.age,
+    location:req.body.location
+  });
 
- debugger;
+  debugger;
 
- user.save().then(
-   (doc)=>{
-    res.send(doc)
-   },
-   (e)=>{
-     res.status(400).send(e);
-   })
-});
+  user.save().then(
+    (doc)=>{
+      res.send(doc)
+    },
+    (e)=>{
+      res.status(400).send(e);
+    })
+  });
 
-app.get('/user', (req,res)=>{
-  User.find().then((users)=>{
-    debugger;
-    res.send({users})
-  },
-   (e)=>{
-     res.send(e).status(400)
-   })
-})
+  app.get('/user', (req,res)=>{
+    User.find().then((users)=>{
+      debugger;
+      res.send({users})
+    },
+    (e)=>{
+      res.send(e).status(400)
+    })
+  })
 
-app.get('/user/:id', (req,res)=>{
-var id = req.params.id;
-if(!ObjectID.isValid(id)) return res.status(404).send();
+  app.get('/user/:id', (req,res)=>{
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)) return res.status(404).send();
 
-User.findById(id).then((doc)=>{if(!doc) return res.status(404).send();
-          res.status(200).send({doc})},
-  (err)=>{
-  return res.status(400).send();
-});
-})
+    User.findById(id).then((doc)=>{if(!doc) return res.status(404).send();
+      res.status(200).send({doc})},
+      (err)=>{
+        return res.status(400).send();
+      });
+    })
+
+    app.delete('/user/:id', (req,res)=>{
+      var id = req.params.id;
+      if(!ObjectID.isValid(id)) return res.status(404).send();
+
+      User.findByIdAndRemove(id).then((doc)=>{if(!doc) return res.status(404).send();
+        res.status(200).send({doc})},
+        (err)=>{
+          return res.status(400).send();
+        });
+      })
 
 
-app.listen(port,()=>{
-  console.log(`server started on port ${port}`);
-})
+    app.listen(port,()=>{
+      console.log(`server started on port ${port}`);
+    })
 
-module.exports = {app};
+    module.exports = {app};
